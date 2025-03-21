4c9b220d9eae8cbacc63720199d66ebf3eacd996
Rhys <98863820+rhysdh540@users.noreply.github.com>
2025-03-21 17:35:57 -0400

    add (experimental) non-toggle mode depends on config existing, will do soon

--- src/lexforge/java/dev/rdh/omnilook/Forgelook.java
+++ src/lexforge/java/dev/rdh/omnilook/Forgelook.java
@@ -4,10 +4,7 @@ import org.apache.commons.lang3.ArrayUtils;
 import org.lwjgl.glfw.GLFW;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraft.client.CameraType;
 import net.minecraft.client.KeyMapping;
@@ -17,7 +14,6 @@ import net.minecraft.client.Minecraft;
 public final class Forgelook extends Omnilook {
 	public final KeyMapping key;
 
-	@SuppressWarnings("removal")
 	public Forgelook() {
 		key = new KeyMapping(KEYBINDING_NAME, GLFW.GLFW_KEY_GRAVE_ACCENT, KeyMapping.CATEGORY_MISC);
 
@@ -26,22 +22,9 @@ public final class Forgelook extends Omnilook {
 			return;
 		}
 
-		// TODO do this with less try catch
-		try {
-			FMLJavaModLoadingContext.get().getModEventBus().addListener(
-					EventPriority.NORMAL,
-					true,
-					RegisterKeyMappingsEvent.class,
-					this::onRegisterKeyMappings
-			);
-		} catch (NoClassDefFoundError e) {
-			// ClientRegistry does the same thing but has a bunch of different package names so it's easier to just do this
-			Minecraft.getInstance().options.keyMappings = ArrayUtils.add(Minecraft.getInstance().options.keyMappings, key);
-		}
-	}
-
-	void onRegisterKeyMappings(Object event) {
-		event.getClass().getDeclaredMethod("register", KeyMapping.class).invoke(event, key);
+		// this is kind of jank but RegisterKeyMappingsEvent doesn't exist until like 1.19
+		// and ClientRegistry (the old way to do it) changed packages a lot
+		Minecraft.getInstance().options.keyMappings = ArrayUtils.add(Minecraft.getInstance().options.keyMappings, key);
 	}
 
 	@Override
