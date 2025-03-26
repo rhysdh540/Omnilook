96eed75a4f6dd575cafdf21d1b1dc270fc7df81c
Rhys <98863820+rhysdh540@users.noreply.github.com>
2025-03-26 10:33:34 -0400

    legacy fabric 7.10+ support

---
+++ src/legacyfabric/java/dev/rdh/omnilook/mixin/legacyfabric/ActiveRenderInfoMixin.java
@@ -0,0 +1,28 @@
+package dev.rdh.omnilook.mixin.legacyfabric;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalFloatRef;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import dev.rdh.omnilook.Omnilook;
+
+import net.minecraft.client.renderer.ActiveRenderInfo;
+import net.minecraft.entity.player.EntityPlayer;
+
+@Mixin(ActiveRenderInfo.class)
+public class ActiveRenderInfoMixin {
+	@Inject(method = "updateRenderInfo", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/ActiveRenderInfo;rotationX:F"))
+	private static void hookRotation(EntityPlayer entity, boolean thirdPerson, CallbackInfo ci,
+									 @Local(ordinal = 2) LocalFloatRef pitch, @Local(ordinal = 3) LocalFloatRef yaw) {
+		Omnilook o = Omnilook.getInstance();
+		o.update();
+
+		if (o.isEnabled()) {
+			pitch.set(o.getXRot());
+			yaw.set(o.getYRot());
+		}
+	}
+}
