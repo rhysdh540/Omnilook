e29a892343ed259c3c9c3fd5b1aa56f52eed024b
Rhys <98863820+rhysdh540@users.noreply.github.com>
2025-03-28 10:13:55 -0400

    i think this is slightly less scuffed

--- src/rift/java/dev/rdh/omnilook/mixin/rift/GameRendererMixin.java
+++ src/rift/java/dev/rdh/omnilook/mixin/rift/GameRendererMixin.java
@@ -1,5 +1,6 @@
 package dev.rdh.omnilook.mixin.rift;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalFloatRef;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,19 +10,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import dev.rdh.omnilook.Omnilook;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.entity.Entity;
 
 // this is extremely scuffed but all the other ways i tried either did nothing or broke the camera spectacularly
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
-	@Inject(method = "orientCamera", at = @At("HEAD"))
-	private void a(float partialTicks, CallbackInfo ci, @Share("yaw") LocalFloatRef yaw, @Share("pitch") LocalFloatRef pitch) {
+	@Inject(method = "orientCamera", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/Minecraft;getRenderViewEntity()Lnet/minecraft/entity/Entity;", shift = At.Shift.AFTER))
+	private void a(float partialTicks, CallbackInfo ci, @Share("yaw") LocalFloatRef yaw, @Share("pitch") LocalFloatRef pitch, @Local Entity entity) {
 		Omnilook o = Omnilook.getInstance();
 		o.update();
 		if (o.isEnabled()) {
-			Entity entity = Minecraft.getInstance().getRenderViewEntity();
 			pitch.set(entity.rotationPitch);
 			yaw.set(entity.rotationYaw);
 
@@ -31,10 +30,9 @@ public class GameRendererMixin {
 	}
 
 	@Inject(method = "orientCamera", at = @At("TAIL"))
-	private void b(float partialTicks, CallbackInfo ci, @Share("yaw") LocalFloatRef yaw, @Share("pitch") LocalFloatRef pitch) {
+	private void b(float partialTicks, CallbackInfo ci, @Share("yaw") LocalFloatRef yaw, @Share("pitch") LocalFloatRef pitch, @Local Entity entity) {
 		Omnilook o = Omnilook.getInstance();
 		if (o.isEnabled()) {
-			Entity entity = Minecraft.getInstance().getRenderViewEntity();
 			entity.rotationPitch = pitch.get();
 			entity.rotationYaw = yaw.get();
 		}
