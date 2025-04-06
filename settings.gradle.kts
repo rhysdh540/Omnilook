a18439c3395c43698a4e81b3f00fe0cf27c6b910
Rhys <98863820+rhysdh540@users.noreply.github.com>
2025-04-05 22:44:59 -0400

    add taugradle compression saves ~2kb 👍

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ pluginManagement.repositories {
     mavenCentral()
     gradlePluginPortal()
     maven("https://maven.fabricmc.net")
+    maven("https://maven.taumc.org/releases")
     maven("https://maven.neoforged.net/releases")
     maven("https://maven.wagyourtail.xyz/releases")
 }
