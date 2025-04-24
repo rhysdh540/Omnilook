25e570bd589866d7bd1aee012438077e4eabdb12
Rhys <98863820+rhysdh540@users.noreply.github.com>
2025-04-24 11:53:58 +0800

    redo log

--- src/main/java/dev/rdh/omnilook/OmniLog.java
+++ src/main/java/dev/rdh/omnilook/OmniLog.java
@@ -1,38 +1,40 @@
 package dev.rdh.omnilook;
 
-import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public final class OmniLog {
-	private static Consumer<String> info, warn, error;
-	private static BiConsumer<String, Throwable> errorWithThrowable;
+	private static final Consumer<String> info, warn, error;
+	private static final BiConsumer<String, Throwable> errorWithThrowable;
 
-	public static void init(String backendName, Consumer<String> info, Consumer<String> warn, Consumer<String> error, BiConsumer<String, Throwable> errorWithThrowable) {
-		if(OmniLog.info != null || OmniLog.warn != null || OmniLog.error != null || OmniLog.errorWithThrowable != null) {
-			error("OmniLog already initialized!");
-			return;
+	static {
+		String backendName;
+		if(MixinPlugin.classExists("org.apache.logging.log4j.Logger")) {
+			org.apache.logging.log4j.Logger logger = org.apache.logging.log4j.LogManager.getLogger("Omnilook");
+			info = logger::info;
+			warn = logger::warn;
+			error = logger::error;
+			errorWithThrowable = logger::error;
+			backendName = "Log4j2";
+		} else if(MixinPlugin.classExists("org.slf4j.Logger")) {
+			org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger("Omnilook");
+			info = logger::info;
+			warn = logger::warn;
+			error = logger::error;
+			errorWithThrowable = logger::error;
+			backendName = "SLF4J";
+		} else {
+			info = s -> System.out.println("[Omnilook/INFO] " + s);
+			warn = s -> System.out.println("[Omnilook/WARN] " + s);
+			error = s -> System.err.println("[Omnilook/ERROR] " + s);
+			errorWithThrowable = (s, e) -> {
+				System.err.println("[Omnilook/ERROR] " + s);
+				e.printStackTrace();
+			};
+			backendName = "fallback";
 		}
 
-		OmniLog.info = Objects.requireNonNull(info);
-		OmniLog.warn = Objects.requireNonNull(warn);
-		OmniLog.error = Objects.requireNonNull(error);
-		OmniLog.errorWithThrowable = Objects.requireNonNull(errorWithThrowable);
-
 		info("OmniLog initialized with " + backendName + " backend");
-	}
-
-	// there's probably a better way of doing this but it works for now
-	public static void initFallback() {
-		init("fallback",
-				s -> System.out.println("[Omnilook/INFO] " + s),
-				s -> System.out.println("[Omnilook/WARN] " + s),
-				s -> System.err.println("[Omnilook/ERROR] " + s),
-				(s, e) -> {
-					System.err.println("[Omnilook/ERROR] " + s);
-					e.printStackTrace();
-				}
-		);
 	}
 
 	public static void info(String message) {
